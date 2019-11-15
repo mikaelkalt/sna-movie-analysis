6bc1677e2a757657479427b82ec730b87cacbfbe
Michael Kalt <4425489+mikaelkalt@users.noreply.github.com>
2019-11-15 17:35:05 +0100

    Add age information for director and actor

--- movies.py
+++ movies.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
+LIMIT = 10000
+
 prefix = """
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
     PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
@@ -19,38 +21,45 @@ prefix = """
 
 movie_query = prefix + """
     
-SELECT ?movie ?name ?duration ?director ?directorLabel  (MIN(YEAR(?publicationDate)) as ?year) (GROUP_CONCAT(DISTINCT (?genre); SEPARATOR=", ") AS ?genres) (GROUP_CONCAT(DISTINCT (?genreLabel); SEPARATOR=", ") AS ?genreLabels)  WHERE
+SELECT ?movie ?name ?duration ?director ?directorLabel (YEAR(?directorBirthDate) as ?directorBirthYear) (MIN(YEAR(?publicationDate)) as ?year) ((?year - ?directorBirthYear) as ?directorAge) 
+(GROUP_CONCAT(DISTINCT (?genre); SEPARATOR=", ") AS ?genres) (GROUP_CONCAT(DISTINCT (?genreLabel); SEPARATOR=", ") AS ?genreLabels)  WHERE
 {
     ?movie wdt:P166 ?award.
     ?award wdt:P31 wd:Q19020.
     ?movie wdt:P31 wd:Q11424.
     ?movie wdt:P1476 ?name.
     OPTIONAL {
-      ?movie wdt:P2047 ?duration.
-      ?movie wdt:P577 ?publicationDate.
-      ?movie wdt:P57 ?director.
-      ?movie wdt:P136 ?genre.
-      SERVICE wikibase:label {
-        bd:serviceParam wikibase:language "en".
-        ?director rdfs:label ?directorLabel.
-        ?genre rdfs:label ?genreLabel
-      }
+        ?movie wdt:P2047 ?duration.
+        ?movie wdt:P577 ?publicationDate.
+        ?movie wdt:P57 ?director.
+        ?director wdt:P569 ?directorBirthDate.
+        ?movie wdt:P136 ?genre.
+        SERVICE wikibase:label {
+            bd:serviceParam wikibase:language "en".
+            ?director rdfs:label ?directorLabel.
+            ?genre rdfs:label ?genreLabel
+        }
     }
 }
-GROUP BY ?movie ?name ?duration ?director ?directorLabel 
-ORDER BY ?movie LIMIT 10000 OFFSET %i"""
+GROUP BY ?movie ?name ?duration ?director ?directorLabel ?directorBirthDate
+ORDER BY ?movie ?director LIMIT """ + str(LIMIT) + """ OFFSET %i"""
 
 actor_query = prefix + """
     
-SELECT ?movie ?actor ?actorName WHERE
+SELECT ?movie ?actor ?actorName (YEAR(?actorBirthDate) as ?actorBirthYear) (MIN(YEAR(?publicationDate)) as ?year) ((?year - ?actorBirthYear) as ?actorAge)  WHERE
 {
 	?movie wdt:P166 ?award.
     ?award wdt:P31 wd:Q19020.
     ?movie wdt:P31 wd:Q11424.
     ?movie wdt:P161 ?actor.
     ?actor wdt:P1477 ?actorName.
+    OPTIONAL {
+      ?movie wdt:P577 ?publicationDate.
+      ?actor wdt:P569 ?actorBirthDate 
+    }
 }
-ORDER BY ?movie LIMIT 10000 OFFSET %i"""
+GROUP BY ?movie ?actor ?actorName ?actorBirthDate
+ORDER BY ?movie ?actor LIMIT """ + str(LIMIT) + """ OFFSET %i"""
 
 def build_movie_query(offset):
     # Not using string.format to avoid escaping the curly braces in query_format
@@ -77,6 +86,8 @@ def parse_movie_results(results, nodes, edges):
         if "director" in result:
             director = result["director"]["value"]
             director_label = result["directorLabel"]["value"]
+            director_birth_year = result["directorBirthYear"]["value"]
+            directing_age = result["directorAge"]["value"]
           
         year = result["year"]["value"] if "year" in result else ""
         duration = result["duration"]["value"] if "duration" in result else ""
@@ -84,9 +95,9 @@ def parse_movie_results(results, nodes, edges):
         nodes[movie] = {'label': label, 'type': 'MOVIE','genre': genres, 'genre_label': genre_labels, 'year': year, 'duration': duration}
 
         if director not in nodes:
-             nodes[director] = {'label': director_label, 'type': 'DIRECTOR'}
+             nodes[director] = {'label': director_label, 'type': 'DIRECTOR', 'year': director_birth_year }
         
-        edges = edges.append({'Source': director, 'Target': movie, 'Label': 'is_directing'}, ignore_index=True)       
+        edges = edges.append({'Source': director, 'Target': movie, 'category': 'is_directing', 'age': directing_age }, ignore_index=True)       
 
     return nodes, edges
 
@@ -95,14 +106,17 @@ def parse_actor_results(results, nodes, edges):
         movie = result["movie"]["value"]
         actor = result["actor"]["value"]
         label = result["actorName"]["value"]
+        
+        acting_age = result["actorAge"]["value"] if "year" in result else 0 
+        actor_birth_year = result["actorBirthYear"]["value"] if "actorBirthYear" in result else 0
           
         if movie not in nodes: 
             print(movie + " was not found before")        
 
         if actor not in nodes:
-             nodes[actor] = {'label': label, 'type': 'ACTOR'}
+             nodes[actor] = {'label': label, 'type': 'ACTOR', 'year': actor_birth_year}
         
-        edges = edges.append({'Source': actor, 'Target': movie, 'Label': 'is_acting'}, ignore_index=True)       
+        edges = edges.append({'Source': actor, 'Target': movie, 'category': 'is_acting', 'age': acting_age }, ignore_index=True)       
 
     return nodes, edges
 
@@ -126,8 +140,8 @@ if __name__ == "__main__":
         results = fetch_data(query)
         nodes, edges = parse_movie_results(results, nodes, edges)
         num_results = len(results["results"]["bindings"])
-        if num_results == 10000:
-            currentOffset += 10000
+        if num_results == LIMIT:
+            currentOffset += LIMIT
         else:
             has_remaining_results = False
     
@@ -138,13 +152,13 @@ if __name__ == "__main__":
         results = fetch_data(query)
         nodes, edges = parse_actor_results(results, nodes, edges)
         num_results = len(results["results"]["bindings"])
-        if num_results == 10000:
-            currentOffset += 10000
+        if num_results == LIMIT:
+            currentOffset += LIMIT
         else:
             has_remaining_results = False
 
 
-    pd_nodes = pd.DataFrame.from_dict(nodes, columns=['label','type', 'duration', 'year', 'genre', 'genre_label'], orient='index')
+    pd_nodes = pd.DataFrame.from_dict(nodes, columns=['label','type', 'year', 'duration', 'genre', 'genre_label'], orient='index')
 
     edges = remove_duplicates(edges)
     write_csvs(pd_nodes)
